2a44e094ca288bda81e0f412fecf0aa36030f33e
Michał Romak <mrblablak@gmail.com>
2017-06-11 13:16:53 +0200

    onmouseover

--- js/6-zdarzenia-events.js
+++ js/6-zdarzenia-events.js
@@ -1,13 +1,30 @@
 'use strict';
-var secondLink = document.getElementsByTagName('a')[1];
+//var secondLink = document.getElementsByTagName('a')[1];
+//
+//console.log(secondLink);
+//
+//function alarm(e){
+//    e.preventDefault();
+//    console.log("kliknięto kolejny link");
+//    
+//    console.log(event);
+//    
+//}
+//secondLink.onclick= alarm;
+//
+//var thirdLink = document.getElementsByTagName('a')[2];
+//thirdLink.addEventListener('click', alarm);
+// thirdLink.removeEventListener('click', alarm);
+var mainHeader = document.getElementById('main-header');
 
-console.log(secondLink);
-
-function alarm(e){
-    e.preventDefault();
-    console.log("kliknięto kolejny link");
-    
-    console.log(event);
-    
+function resize(e){
+//    console.log(e.type);
+    if(e.type == 'mouseover'){
+        mainHeader.style.fontSize="70px";
+    }
+    else{
+        mainHeader.style.fontSize = "inherit";
+    }
 }
-secondLink.onclick= alarm;
+mainHeader.onmouseover =resize;
+mainHeader.onmouseout =resize;
