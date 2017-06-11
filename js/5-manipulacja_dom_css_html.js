9470a5f563a0f429fd94b1e64b51e3df53be02fc
Michał Romak <mrblablak@gmail.com>
2017-06-11 11:34:35 +0200

    manipulacja_dom_css_html

--- js/5-manipulacja_dom_css_html.js
+++ js/5-manipulacja_dom_css_html.js
@@ -3,3 +3,13 @@ var mainHeader = document.getElementById('main-header');
 console.log(mainHeader);
 
 mainHeader.innerHTML= "treść nagłówka";
+
+var link = document.getElementsByClassName('link')[0];
+
+console.log(link);
+link.href = "http://akademia108.pl";
+
+link.className="nowa-klasa";
+mainHeader.style.color ='#11aa22';
+link.text='ddd';
+mainHeader.style.color='#11ff22'
