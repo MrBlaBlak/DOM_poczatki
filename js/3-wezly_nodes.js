6e5924f4fe1b3d83fcfedf8616c533cbce2e60e5
Michał Romak <mrblablak@gmail.com>
2017-06-11 10:37:22 +0200

    wezlu_nodes

---
+++ js/3-wezly_nodes.js
@@ -0,0 +1,16 @@
+'use strict';
+var pierwszyDiv = document.getElementById('parFirst');
+/*console.log(pierwszyDiv);
+console.log(pierwszyDiv.parentNode);
+console.log(pierwszyDiv.parentElement);*/
+//console.log(pierwszyDiv.childNodes);
+//console.log(pierwszyDiv.childNodes[3]);
+//
+//console.log(pierwszyDiv.children);
+console.log(pierwszyDiv.firstChild);
+console.log(pierwszyDiv.lastChild);
+
+//var pierwszyDiv = null; usuwa
+var pierwszyChild = pierwszyDiv.childNodes[2];
+console.log(pierwszyChild);
+console.log(pierwszyChild.nextSibling.nodeType);
