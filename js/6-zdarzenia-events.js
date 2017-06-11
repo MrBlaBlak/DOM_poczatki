be89c23485840ebf32ff54300a93db654c0266fb
Michał Romak <mrblablak@gmail.com>
2017-06-11 13:35:17 +0200

    koniec

--- js/6-zdarzenia-events.js
+++ js/6-zdarzenia-events.js
@@ -15,16 +15,26 @@
 //var thirdLink = document.getElementsByTagName('a')[2];
 //thirdLink.addEventListener('click', alarm);
 // thirdLink.removeEventListener('click', alarm);
-var mainHeader = document.getElementById('main-header');
-
-function resize(e){
-//    console.log(e.type);
-    if(e.type == 'mouseover'){
-        mainHeader.style.fontSize="70px";
-    }
-    else{
-        mainHeader.style.fontSize = "inherit";
-    }
+//var mainHeader = document.getElementById('main-header');
+//
+//function resize(e){
+////    console.log(e.type);
+//    if(e.type == 'mouseover'){
+//        mainHeader.style.fontSize="70px";
+//    }
+//    else{
+//        mainHeader.style.fontSize = "inherit";
+//    }
+//}
+//mainHeader.onmouseover =resize;
+//mainHeader.onmouseout =resize;
+function klikHeader(){
+    console.log("kliknąłeś w header");
 }
-mainHeader.onmouseover =resize;
-mainHeader.onmouseout =resize;
+document.getElementsByTagName('header')[0].onclick = klikHeader;
+function klikH1(e){
+    e.stopPropagation();
+    console.log("kliknałeś w h1");
+    
+}
+document.getElementsByTagName('h1')[0].onclick=klikH1;
